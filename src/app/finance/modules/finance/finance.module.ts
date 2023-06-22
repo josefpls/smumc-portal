110a455f1fff84be16de13ee15927f41c31d9431
josefpls <mjoseph.publico@gmail.com>
2023-06-22 20:17:39 +0800

    create finance module

---
+++ src/app/finance/modules/finance/finance.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from 'src/app/shared/modules/material/material.module';
+
+import { FinanceDashboardComponent } from '../../components/finance-dashboard/finance-dashboard.component';
+import { FinanceComponent } from '../../components/finance/finance.component';
+import { FinanceRecordsComponent } from '../../components/finance-records/finance-records.component';
+import { FinanceFormComponent } from '../../components/finance-form/finance-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+const financeRoutes: Routes = [
+	{ path: "", redirectTo: "manage", pathMatch: "full" },
+	{ path: "manage", component: FinanceComponent },
+    { path: "form", component: FinanceFormComponent }
+]
+
+@NgModule({
+	declarations: [
+		FinanceDashboardComponent,
+		FinanceComponent,
+        FinanceRecordsComponent,
+        FinanceFormComponent
+    ],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(financeRoutes),
+        MaterialModule,
+        FormsModule, ReactiveFormsModule
+    ],
+    exports: [
+        FinanceFormComponent
+    ]
+})
+export class FinanceModule { }
