bdfc175b3a6117d8393398e74e9f5c1a644ab414
josefpls <mjoseph.publico@gmail.com>
2023-06-22 20:17:03 +0800

    Use snackbars for notifications

--- src/app/authentication/components/login/login.component.ts
+++ src/app/authentication/components/login/login.component.ts
@@ -1,46 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { FirebaseError } from 'firebase/app';
 import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  loginForm!: FormGroup;
-  error: string = "";
+    loginForm!: FormGroup;
 
-  constructor(
-    private _authService: AuthenticationService,
-    private _router: Router,
-  ) { }
+    constructor(
+        private _authService: AuthenticationService,
+        private _router: Router,
+        private _snackbar: MatSnackBar,
+    ) { }
 
-  ngOnInit(): void {
-    this.initializeForm();
-  }
+    ngOnInit(): void {
+        this.initializeForm();
+    }
 
-  initializeForm(): void {
-    this.loginForm = new FormGroup({
-      email: new FormControl<string>("", [Validators.required, Validators.email]),
-      password: new FormControl<string>("", [Validators.required, Validators.minLength(8)]),
-    })
-  }
+    initializeForm(): void {
+        this.loginForm = new FormGroup({
+            email: new FormControl<string>("", [Validators.required, Validators.email]),
+            password: new FormControl<string>("", [Validators.required, Validators.minLength(8)]),
+        })
+    }
 
-  submitForm(): void {
-    if(!this.loginForm.valid) return;
-    this._authService.login(this.loginForm.value["email"], this.loginForm.value["password"]).then((credentials) => {
-      localStorage.setItem("user", credentials["user"]["uid"]);
-      this._router.navigate(["/main"]);
-    }).catch((error: FirebaseError) => {
-      this.error = error["code"];
-      setTimeout(() => {
-        this.error = "";
-      }, 3000);
-    });
-  }
+    submitForm(): void {
+        if(!this.loginForm.valid) return;
+        this._authService.login(this.loginForm.value["email"], this.loginForm.value["password"]).then((credentials) => {
+            localStorage.setItem("user", credentials["user"]["uid"]);
+            this._snackbar.open("Welcome " + credentials["user"]["email"], "Close", {
+                duration: 2000
+            });
+            this._router.navigate(["/main"]);
+        }).catch((error: FirebaseError) => {
+            const readableError: string = error["code"].split("/")[1].replaceAll("-", " ");
+            this._snackbar.open(readableError, "Close", {
+                duration: 2000
+            });
+        });
+    }
 
 }
